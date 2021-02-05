f64a82d82d62fa19dc20abbc6846a9775628f80f
Stan Ooms <stanooms96@gmail.com>
2021-02-06 00:17:42 +0100

    fixed cache
    now retrieving data from cache if query exists as key

--- project/api.js
+++ project/api.js
@@ -11,7 +11,7 @@ const onfulfilled = function (err, data, query) {
     return;
     // stoppen op error
   }
-  console.log(data);
+
   // verder met data
   data = dataRefine.checkQuery(data, query);
   localStorage.storeInitList(data, query); // store in cache ->localStorage
@@ -29,53 +29,15 @@ class Request {
 
     fetch(`${endPoint}`)
       .then((res) => {
-        console.log(res);
         return res.json();
       })
       .then((data) => {
-        // console.log(data.Data);
         this.data = data.Data;
         onfulfilled(null, this.data, this.query);
       })
       .catch((err) => {
         onfulfilled(err);
       });
-
-    // function getData() {
-    //   console.log("request fired");
-    //   if (query) {
-    //     // check request reason
-
-    //     // fetch basic infomation of all cryptocurrency
-    //     async function x() {
-    //       const response = await fetch(endPoint);
-    //       return response.json();
-    //     }
-    //     x().then((res) => {
-    //       this.data = res;
-    //
-    //       if (this.query === "initList") {
-    //         const responseWithQuery = {
-    //           res: res,
-    //           query: query, // add query as a property of a new object
-    //         };
-    //         // console.log(responseWithQuery);
-    //         events.dataArrived(responseWithQuery);
-    //       }
-
-    //       if (this.query === "topListByMarketCapOverview") {
-    //         const responseWithQuery = {
-    //           res: res,
-    //           query: query, // add query as a property of a new object
-    //         };
-    //         // console.log(responseWithQuery);
-    //         events.dataArrived(responseWithQuery);
-    //       }
-    //     });
-    //   } else {
-    //     console.error("no/invalid query");
-    //   }
-    // }
   }
 }
 
--- project/cache.js
+++ project/cache.js
@@ -2,24 +2,21 @@ import events from "./events.js";
 
 const localStorage = {
   init: (query) => {
-    //query undefined?
-
-    if (!window.localStorage.getItem(query)) {
-      // console.log(window.localStorage.getItem(query));
-
+    //query undefined
+    const cache = window.localStorage.getItem(query);
+    console.log(window.localStorage.getItem(query));
+    if (cache === undefined || null) {
       // No data has been stored to localStorage so far. So we do this now!
+
       events.retrieveInitLists();
     } else {
-      // Data was stored previously and we will retrieve it.
+      console.log("retrieve data from cache");
       const data = window.localStorage.getItem(query);
-      // console.log(JSON.parse(data));
     }
   },
   storeInitList: (data, query) => {
-    // console.log(data);
-    // console.log(query);
     window.localStorage.setItem(query, JSON.stringify(data));
-    console.log("Stored Initial List");
+    console.log(`chached: ${query}`);
   },
 };
 
