079c7014fa10bc644d1fa33c62ea189697a67f62
Stan Ooms <stanooms96@gmail.com>
2021-02-07 01:31:56 +0100

    Update app.js

--- project/app.js
+++ project/app.js
@@ -1,7 +1,9 @@
 import events from "./events.js";
 import localStorage from "./cache.js";
 (function () {
+  // add event listener
   events.submitBtn();
+  // activate localStorage
   localStorage.init();
   console.log("running app.js");
 })();
