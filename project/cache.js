493e203b531214b8da7095645c0a06884eea4741
Stan Ooms <stanooms96@gmail.com>
2021-02-08 01:41:26 +0100

    Update cache.js

--- project/cache.js
+++ project/cache.js
@@ -15,25 +15,37 @@ const localStorage = {
       } else {
         if (endPoint.query === "topListByMarketCapOverview") {
           const data = templator.createDataSetMarketCapOverview(endPoint.query); // Get Data
+
           //console.log(data);
           // in elke coin kijken
           data.forEach((coin, i) => {
-            let element = document.querySelectorAll(".data")[i]; // select HTML Element to render
-            let parentElement = element.parentNode;
-            console.log(parentElement);
+            let element = document.querySelectorAll(".data"); // select HTML Element to render
+            console.log(i);
             console.log(element);
-            let dataHMTLString = element.innerHTML.toString(); // Create Template
+            console.log(document.querySelectorAll(".data")[0]);
+            const templatingLayout = document.querySelectorAll(".data")[0];
+            console.log(templatingLayout);
+            let parentElement = document.querySelector("tbody"); // select parent
+            let dataHMTLString = element[i].innerHTML.toString(); // Create Template
             const renderedElement = render.render(dataHMTLString, coin); // create next coin layout
             const newRowElement = document.createElement("tr"); // create new Row Element
+            newRowElement.setAttribute("id", `data${i}`);
+            console.log(parentElement);
+
             newRowElement.innerHTML = renderedElement; // set innerHTML to ne
             console.log(dataHMTLString);
             console.log(renderedElement);
-            element = element.insertAdjacentElement("beforeend", newRowElement);
-
+            parentElement.insertAdjacentElement("beforeend", newRowElement);
+            console.log(document.querySelectorAll(".data")[0].innerHTML);
             // element.insertAdjacentElement("before");
             // render.render(Tempplate(dataHMTLString), daco=ta)
             //console.log(coin);
             // const template = render.Template(coin.)
+            console.log(element);
+            console.log(parentElement.children[i]);
+            element = parentElement.removeChild(parentElement.children[i]);
+            parentElement.appendChild(templatingLayout);
+            console.log(element);
           });
           // events.bodyIsLoaded(data);
         }
